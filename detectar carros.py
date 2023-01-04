16218d5610f0a3bd4eb5bdc4587dfab919d91004
marlonvl <72558072+marlonvludwig@users.noreply.github.com>
2023-01-04 19:47:09 -0300

    Update detectar carros.py

--- detectar carros.py
+++ detectar carros.py
@@ -4,15 +4,11 @@ import time
 import cv2
 
 # Model
-#model = torch.hub.load('ultralytics/yolov5', 'yolov5x')  # or yolov5n - yolov5x6, custom
-#model = yolov5.load('yolov5s.pt') #2.08 0.66  #0.49
-model = yolov5.load('yolov5x.pt') #0,49
+model = yolov5.load('yolov5s.pt') 
 
 # Images
-img = 'test.jpg'  # or file, Path, PIL, OpenCV, numpy, list
-
+img = ('teste.jpg')  # or file, Path, PIL, OpenCV, numpy, list
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
 
 tempo1 = time.time()
 # Inference
@@ -23,20 +19,25 @@ print("Tempo decorrido: ")
 print(tempo)
 
 # Results
-results.show()  # or .show(), .save(), .crop(), .pandas(), etc.
+#results.show()  # or .show(), .save(), .crop(), .pandas(), etc.
 print(results.xyxy[0][0])
 print(len(results.xyxy[0][0])) #número de colunas
 print(len(results.xyxy[0])) #número de linhas
-cv2.imwrite('Resultado2.jpg', results.save())
+cv2.imwrite('Resultado.jpg', results.save())
 
-#objetos = len(results.xyxy) #quantos objetos achados : carros, person etc
-#print(objetos)
-#for results in range(objetos):
-#    quantidade = len(results.xyxy[0]) #quanto de cada objeto em cada posição
-#    print(quantidade)
-    
-    
-   #for quantidadeObjeto in results
-   #     __,__,__,__,confianca,classe = results.xyxy[objetos][1].numpy() #salva valores de % e tipo de objeto
-   #     nomeClasse = results.names[int(classe)] #pega o tipo da classe
-   #     if nomeClasse == "car"
+   
+cont = 0
+if (len(results.xyxy[0]) >= 0):
+    while(cont < (len(results.xyxy[0]))):
+        __,__,__,__,confianca,classe = results.xyxy[0][cont].numpy() #salva valores de % e tipo de objeto
+        numeroClasse = int(classe) #pega o número do tipo da classe
+        print("Numero da classe: ",numeroClasse)
+        nomeClasse = results.names[int(classe)] #pega o tipo da classe
+        print("Nome da classe: "+nomeClasse+" | Confiança: "+str(confianca))
+        if nomeClasse == "car":
+            print("############# Tem carro! #############")
+        else:
+            print("############# Não tem carro! #############")
+        cont += 1
+else:
+    print("############# Não tem carro! #############")
